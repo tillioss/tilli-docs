0afd48f33fe99dd5fd22666909ce939b4655e32c
Hannah George <108776565+Hannah31George@users.noreply.github.com>
2022-12-05 15:19:57 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/tillioss/tilli-docs',
+            href: 'https://github.com/tillioss',
             label: 'GitHub',
             position: 'right',
           },
