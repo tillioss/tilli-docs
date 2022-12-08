b676e01982de7735a4a79328ae90ba1ff0b3ea01
BrianDsouza <briandsouza17@gmail.com>
2022-12-08 17:44:39 +0530

    Style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,37 +48,28 @@ export default function Home() {
           <img loading="lazy" src={Information1} alt="Information1" title="Information1" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" />
         </p>
         <div style={{textAlign: "center"}}>
-        <button type="button" style={{backgroundColor: 'yellowgreen'}} ><a href="https://tilli.teqbahn.com/tilli-web" target="_blank"><h1>     Play     </h1></a></button>
+        <button className={styles.Buttonclass} type="button" style={{backgroundColor: '#FAD202'}} ><a href="https://tilli.teqbahn.com/tilli-web" target="_blank"><h1>     Play     </h1></a></button>
         </div>
 
         <p style={{ textAlign: "center" }}>
           <img loading="lazy" src={Information2} alt="Information2" title="Information2" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" />
         </p>
         <div style={{textAlign: "center"}}>
-        <button type="button" style={{backgroundColor: 'yellowgreen'}} ><a href="https://github.com/tillioss/tilli-ide" target="_blank"><h1>   IDE     </h1></a></button>
-        <button type="button" style={{backgroundColor: 'yellowgreen'}}><a href="https://github.com/tillioss/Godot-Strategies-V1" target="_blank"><h1>    Tilli x Godot     </h1></a></button>
+        <button className={styles.Buttonclass} type="button" style={{backgroundColor: '#FAD202'}}><a href="https://github.com/tillioss/tilli-ide" target="_blank"><h1>   IDE     </h1></a></button>
+        <button className={styles.Buttonclass} type="button" style={{backgroundColor: '#FAD202'}}><a href="https://github.com/tillioss/Godot-Strategies-V1" target="_blank"><h1>    Tilli x Godot     </h1></a></button>
         </div>
-
-
-
         <p style={{ textAlign: "center" }}>
           <img loading="lazy" src={Information3} alt="Information3" title="Information3" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" />
         </p>
         <div style={{textAlign: "center"}}>
-        <button type="button" style={{backgroundColor: 'yellowgreen'}} ><a href="https://tillioss.github.io/docs/project-charter/" target="_blank"><h1>Project Charter</h1></a></button>
+        <button className={styles.Buttonclass} type="button" style={{backgroundColor: '#FAD202'}} ><a href="https://tillioss.github.io/docs/project-charter/" target="_blank"><h1>Project Charter</h1></a></button>
         </div>
-
-
-
         <p style={{ textAlign: "center" }}>
           <img loading="lazy" src={Information4} alt="Information4" title="Information4" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" />
         </p>
         <div style={{textAlign: "center" }}>
-        <button type="button" style={{backgroundColor: 'yellowgreen'}} ><a href="https://github.com/tillioss" target="_blank"><h1> Github </h1></a></button>
+        <button className={styles.Buttonclass} type="button" style={{backgroundColor: '#FAD202'}} ><a href="https://github.com/tillioss" target="_blank"><h1> Github </h1></a></button>
         </div>
-
-        
-
         <p style={{ textAlign: "center" }}>
           <img loading="lazy" src={TilliLogo} alt="Tilli Logo" title="tilli logo" class="img_node_modules-@docusaurus-theme-classic-lib-theme-MDXComponents-Img-styles-module" />
         </p>
