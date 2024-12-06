0d3f2fa037a6c840a4cfb003851b4a5bcb7ff492
anjula-sack <anjulashanaka@gmail.com>
2024-12-06 09:27:31 +0530

    Update the deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,40 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Tilli Docs',
-  tagline: 'Tilli Docs',
-  url: 'https://tillioss.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/tilli.png',
+  title: "Tilli Docs",
+  tagline: "Tilli Docs",
+  url: "https://tillioss.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/tilli.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'tillioss', // Usually your GitHub org/user name.
-  projectName: 'tillioss.github.io', // Usually your repo name.
-  deploymentBranch:'main',
+  organizationName: "tillioss", // Usually your GitHub org/user name.
+  projectName: "tillioss.github.io", // Usually your repo name.
+  deploymentBranch: "master",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -48,7 +48,7 @@ const config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -58,28 +58,28 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Tilli',
+        title: "Tilli",
         logo: {
-          alt: 'tilli  Logo',
-          src: 'img/tilli.png',
+          alt: "tilli  Logo",
+          src: "img/tilli.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'project-charter',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "project-charter",
+            position: "left",
+            label: "Docs",
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/tillioss',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/tillioss",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           // {
           //   title: 'Docs',
